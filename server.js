51057bc52d14f71de9f51f0b972addfb86c5433f
C:\Users\HP <hyllusx@gmail.com>
2026-01-19 07:49:55 +0200

    Create PORT environment variable

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import homeRouter from './routes/home.routes.js'
+import 'dotenv/config'
 
 const app = express()
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
-app.use('/home', homeRouter)
+app.use('/api/home', homeRouter)
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.listen(PORT, () => console.log('Listening on port ' + PORT))
